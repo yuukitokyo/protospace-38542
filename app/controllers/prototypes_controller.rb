212c81adbde6bd791d3e28c11b0b1b8df3e72235
yuukitokyo <yamadayuuki2678@gmail.com>
2023-01-22 12:29:48 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,61 @@
+class PrototypesController < ApplicationController
+before_action :authenticate_user!,  except: [:index, :show]
+before_action :move_to_index, only: [:edit, :update, :destroy]
+
+
+def index
+  @prototypes = Prototype.includes(:user)
+end
+
+def new
+  @prototype = Prototype.new
+end
+
+def create
+   @prototype = Prototype.new(prototype_params)
+  if @prototype.save
+     redirect_to root_path
+  else
+    render :new
+  end
+end
+
+def show
+  @prototype = Prototype.find(params[:id])
+  @comment = Comment.new
+  @comments = @prototype.comments
+end
+
+def edit
+  @prototype = Prototype.find(params[:id])
+end
+
+def update
+  if @prototype.update(prototype_params)
+     redirect_to prototype_path
+  else
+     render :edit
+  end
+end
+
+def destroy
+  if @prototype.destroy
+     redirect_to root_path
+  else
+     redirect_to root_path
+  end
+end
+
+
+private
+
+def prototype_params
+  params.require(:prototype).permit(:title, :catch_copy, :concept ,:image).merge(user_id: current_user.id)
+end
+
+def move_to_index
+  @prototype = Prototype.find(params[:id])
+  redirect_to root_path unless current_user == @prototype.user
+end
+
+end
